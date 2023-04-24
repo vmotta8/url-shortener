0a87eac3e4f50d3498686523643b6bf904215449
Matheus Leite <leitmatheus@gmail.com>
2023-04-24 20:12:27 -0300

    feat: shortener URL updated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 def shorten_url(full_url):
-  return full_url
+  url_hash = "123456"  
+  return f"https://shorturl.com/{url_hash}"
 
 input_url = input("Enter a URL: ")
 short_url = shorten_url(input_url)
